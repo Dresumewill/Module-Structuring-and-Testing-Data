3f23c0faf5bb79912f1e97165cb706f01d67d07b
Ifeyinwa <ifeyinwaofu@gmail.com>
2025-12-07 01:10:15 +0100

    variables to store dir created

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -10,14 +10,15 @@
 // (All spaces in the "" line should be ignored. They are purely for formatting.)
 
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
+
 const lastSlashIndex = filePath.lastIndexOf("/");
 const base = filePath.slice(lastSlashIndex + 1);
+
 console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = ;
-const ext = ;
-
-// https://www.google.com/search?q=slice+mdn
+const dir = filePath.slice(0, lastSlashIndex + 1);
+const lastDotIndex = filePath.lastSlashIndexOf(".");
+const ext = filePath.slice(lastDotIndex);
