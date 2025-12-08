252b75d8af894dd2b7e87ebadfdede83ac8a0dfe
Ifeyinwa <ifeyinwaofu@gmail.com>
2025-12-08 18:55:58 +0100

    a syntaxError was identified and i have to renamed the variable to start with a letter

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,9 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+// const 12HourClockTime = "20:53"; // Error: variable name can not start with a number
+// const 24hourClockTime = "08:53"; // Error: variable name can not start with a number
+
+
+// This is a syntaxError: invalid or unexpected token
+// To start, the Variable has to be renamed with a letter on both
+
+const time24Hour = "20:53";
+const time12Hour = "08:53";
