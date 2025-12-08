abe97244a07633a137d693f0752b5eb432b76d19
Ifeyinwa <ifeyinwaofu@gmail.com>
2025-12-08 21:28:15 +0100

    identifying the functional call in the file

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -12,8 +12,20 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
+/* there are 5 functional call in this file, and they´re on the following line: Line 4: replaceAll, Line 5: replaceAll, Line 10: console.log
+carPrice = Number(carPrice.replaceAll(",", ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
+Number(...), carPrice.replaceAll(...)
+Number(...), priceAfterOneYear.replaceAll(...)
+console.log(\The percentage change is ${percentageChange}`);`*/
 
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+/*After running the code, the error is identified in line 5:
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+The error is a SyntaxError because the arguments for the replaceAll() method are not correctly separated. It should be replaceAll(searchValue, replaceValue).
+*/
+//To fix insert the required comma separator
+/* priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 // c) Identify all the lines that are variable reassignment statements
 
