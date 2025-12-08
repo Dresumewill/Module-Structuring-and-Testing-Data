6c502789b3813bd427552cbb4faf59f834afdbd8
Ifeyinwa <ifeyinwaofu@gmail.com>
2025-12-08 12:59:05 +0100

    single-line comment marker was added to make it a valid javascript syntax

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,5 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+// This is just an instruction for the first activity - but it is just for human consumption
+// We don't want the computer to run these 2 lines - how can we solve this problem?
+
+// this text is not a valid javascript syntax, which could cause a syntaxError
+// to fix this, a single-line comment marker (//) should be at the beginning of each line.
