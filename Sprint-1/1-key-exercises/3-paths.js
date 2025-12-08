02524c7a131288b4de3feacabecfa10ea551539a
Ifeyinwa <ifeyinwaofu@gmail.com>
2025-12-08 11:34:24 +0100

    variable to store the ext part was created

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -14,11 +14,15 @@ const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
 const lastSlashIndex = filePath.lastIndexOf("/");
 const base = filePath.slice(lastSlashIndex + 1);
 
-console.log(`The base part of ${filePath} is ${base}`);
+// console.log(`The base part of ${filePath} is ${base}`);
+
+// Create a variable to store the ext part of the variable
+let indexOfTheBase = base.lastIndexOf(".")
+console.log(base);
+
+let ext = base.slice(indexOfTheBase);
+console.log(ext);
+console.log("-----------------------------------------------------------")
 
 // Create a variable to store the dir part of the filePath variable
-// Create a variable to store the ext part of the variable
 
-const dir = filePath.slice(0, lastSlashIndex + 1);
-const lastDotIndex = filePath.lastSlashIndexOf(".");
-const ext = filePath.slice(lastDotIndex);
