a5f55e7d51ad2e53e1c50d616120d631c020a0fd
Ifeyinwa <ifeyinwaofu@gmail.com>
2025-12-09 08:20:17 +0100

    my interpretation of line 4: const totalMinutes

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -8,6 +8,11 @@ const totalHours = (totalMinutes - remainingMinutes) / 60;
 
 const result = `${totalHours}:${remainingMinutes}:${remainingSeconds}`;
 console.log(result);
+console.log(movieLength)
+console.log(remainingSeconds)
+console.log(totalMinutes)
+console.log(remainingMinutes)
+console.log(totalHours)
 
 // For the piece of code above, read the code and then answer the following questions
 
@@ -22,12 +27,20 @@ v.  const totalHours
 vi. const result */
 
 // b) How many function calls are there?
+/* There is only 1 function call in the program and the call is console.log(result)*/
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
+/* This computes the remainder left over when one operand (movieLength) is divided by a second operand (60)
+It is the number of seconds remaining after the total second (movieLength) have been converted into whole minute.
+meanwhile they are 60 seconds in a minute, dividing by 60 and taking the remainder will give the seconds that don´t make up a full minute. */
 
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
+/* Line 4 is const totalMinutes = (movieLength - remainingSeconds) / 60;
+The expression calculates the total number of whole minutes contained in the (movieLength)
+i.e movieLength - remainingSeconds which subtraction removes the partial seconds, then leaving a value that is divisible perfectly by 60. 
 
+(...) / 60; This division converts the total seconds into total minutes. */ 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
