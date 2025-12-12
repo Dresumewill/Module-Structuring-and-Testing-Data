bf28809859461c7a45e37bd0de323ad73d09fc13
Ifeyinwa <ifeyinwaofu@gmail.com>
2025-12-12 21:47:19 +0100

    predicting and explaning the code

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,21 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> I predict the code will result in a Syntax Error that halts execution before the function can even be called. 
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
+// function capitalise(str) {
+  // let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  // return str;
+// }
+
+// =============> The error occurs because the variable name str is used twice: first as a function parameter ( function capitalise()), and second, as a new variable declared inside the function(let str = ...).
+// The let keyword prevents redeclaration in the same scope, leading to a SyntaxError.
+// =============> write your new code here
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  // Remove ¨let¨ keyword to reassign the existing ¨str¨
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str; 
 }
 
-// =============> write your explanation here
-// =============> write your new code here
+console.log(capitalise("hello world"));
