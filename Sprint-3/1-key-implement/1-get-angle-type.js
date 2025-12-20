d1d8245a0d92556b4e180bb6efcd9bf6d545ee8e
Ifeyinwa <ifeyinwaofu@gmail.com>
2025-12-20 18:50:44 +0100

    just implemented the function getAngleType and testing the function

--- Sprint-3/1-key-implement/1-get-angle-type.js
+++ Sprint-3/1-key-implement/1-get-angle-type.js
@@ -11,7 +11,8 @@ function getAngleType(angle) {
    if (angle === 90) return "Right angle";
    if (angle < 90) return "Acute angle";
    if (angle < 180) return "Obtuse angle";
-   if (angle === 180) return "Straight angle";
+   if (angle === 180) return "Straight angle"; 
+   if (angle > 180 && angle < 360) return "Reflex angle";
    // read to the end, complete line 36, then pass your test here
 }
 
@@ -55,6 +56,9 @@ assertEquals(obtuse, "Obtuse angle");
 // ====> write your test here, and then add a line to pass the test in the function above
 const Straight = getAngleType(180);
 assertEquals(Straight, "Straight angle");
+
 // When the angle is greater than 180 degrees and less than 360 degrees,
 // Then the function should return "Reflex angle"
 // ====> write your test here, and then add a line to pass the test in the function above
+const Reflex = getAngleType(300);
+assertEquals(Reflex, "Reflex angle");
