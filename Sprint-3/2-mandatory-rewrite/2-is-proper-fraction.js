abbb53eb9ff018abfba047b9a6e7208742032443
Ifeyinwa <ifeyinwaofu@gmail.com>
2025-12-21 18:07:12 +0100

    function module.export

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,6 +1,10 @@
 function isProperFraction(numerator, denominator) {
     if (numerator < denominator) return true;
-    // add your completed function from key-implement here
+    if (numerator > denominator) return false;
+    if (numerator < denominator) return true;
+    if (numerator === denominator) return false;
+    if (numerator < denominator) return true;
+    if (numerator = denominator) return true;
 }
 
 module.exports = isProperFraction;
