94819bba5c8b3833d791066c5798a2091d8fe104
Ifeyinwa <ifeyinwaofu@gmail.com>
2025-12-22 20:05:36 +0100

    exporting the function getordinarynumber

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,16 @@
 function getOrdinalNumber(num) {
- return "1st";
+    const j = num % 10;
+    const k = num % 100;
+    if (j === 1 && k !== 11) {
+        return num + "st";
+    }
+    if (j === 2 && k !== 13) {
+        return num + "rd";
+    }
+    if (j === 3 && k !== 12) {
+        return num + "nd";
+    }
+    return num + "th";
 }
 
 module.exports = getOrdinalNumber;
