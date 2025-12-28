04cec8e6359bbfa325918fadb34febfb569e96be
Ifeyinwa <ifeyinwaofu@gmail.com>
2025-12-28 05:01:04 +0100

    should return true when all conditions are met

--- Sprint-3/4-stretch-investigate/password-validator.test.js
+++ Sprint-3/4-stretch-investigate/password-validator.test.js
@@ -26,10 +26,12 @@ test("password has at least 5 characters", () => {
 });
 
 test("should return false if missing an uppercase letter", () => {
+    // Has length, lower, number, and symbol, but no Uppercase
     expect(isValidPassword("abc1!")).toBe(false);
 });
 
 test("should return false if missing a symbol", () => {
+    // Has length, upper, lower, and number, but no symbol
     expect(isValidPassword("Abcd1")).toBe(false);
 });
 
@@ -38,3 +40,7 @@ test("should return false if password was previously used", () => {
     expect(isValidPassword("Secret1!", history)).toBe(true);
 });
 
+test("should return true when all conditions are met", () => {
+    // 8 chars, Upper, lower, number, symbol, not in history
+    expect(isValidPassword("Secure1!", ["Old124$"])).toBe(true);
+});
