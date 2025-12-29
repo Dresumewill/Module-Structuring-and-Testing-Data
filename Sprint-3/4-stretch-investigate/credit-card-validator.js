6069d7d34c93da3cf4914631302826a04c24c97f
Ifeyinwa <ifeyinwaofu@gmail.com>
2025-12-29 21:11:04 +0100

    validating credit card

---
+++ Sprint-3/4-stretch-investigate/credit-card-validator.js
@@ -0,0 +1,19 @@
+// Rule 1: Number must be exactly 16 digits
+function validateCreditCard(cardNumber) {
+    if (cardNumber.length !== 16) {
+        return false;
+    }
+
+    let sum = 0;
+    let allDigitsAreSame = true;
+
+    // loop through the card number once to check for invalid characters, calculate the sum, and check for diversity
+    for (let i = 0; i < cardNumber.length; i++) {
+        const currentDigit = cardNumber[i];
+        // Check if the character is a valid number (0-9) 
+        // if we find a letter or symbol, return false
+        if (currentDigit < `0` || currentDigit > `9`) {
+            return false;
+        }
+    }
+}
